491bb4b67af1980148aa6988bc9b95091daa0a74
ryi2526 <47389471+ryi2526@users.noreply.github.com>
2019-03-22 21:30:51 +0900

    003

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,19 @@ class SessionsController < ApplicationController
 
   end
 
+  def okdesu
+
+  end
+
   private
     def set_ok
       @id = params[:id]
       @password = params[:password]
       #ユーザーの存在有無（IDとパスワードの一致）
-      #if User.find(@id) == User.find('1')
-      #  render 'sessions/ok'
-      #else
-      #  render 'test/index'
-      #end
+      if User.where(["id = ? and password = ?", params[:id], params[:password]]).count == 1
+        redirect_to '/okdesu'
+      else
+        redirect_to '/test'
+      end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   resources :diaries
   get '/login', to: 'sessions#login'
-  get '/ok', to: 'session#ok'
+  get '/ok', to: 'sessions#ok'
   post '/login', to: 'sessions#ok'
+  get '/okdesu', to: 'sessions#okdesu'
   # For details on the DSL available within this file,see http://guides.rubyonrails.org/routing.html
 
   # 　リンク　　to:  コントローラー#アクション
   #   リンク先のビューにアクションを使用して値の受け渡しを行うために操作
   get '/test', to: 'test#index'
-
   #   リンク
   #   コントローラーを使用せずリンクのビューを表示する
   get '/test/new'
